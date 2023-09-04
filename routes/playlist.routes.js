ac3eb96f65df4b717c64ed43fcd0ab19ea3d2304
kekonline <kekonline@gmail.com>
2023-09-04 22:30:50 +0200

    some stuff added

--- routes/playlist.routes.js
+++ routes/playlist.routes.js
@@ -1,36 +1,75 @@
 const router = require("express").Router();
-const Playlist = require("../models/Playlist.model")
-const isAuthenticated = require("../middlewares/isAuthenticated")
+const Playlist = require("../models/Playlist.model");
+const isAuthenticated = require("../middlewares/isAuthenticated");
+
+
 // GET /api/playlist/all/:childid - Get all playlist of a specific child
 router.get("/all/:childid", isAuthenticated, async (req, res, next) => {
-
-    console.log("child if", req.params.childid);
-
+    // console.log("child if", req.params.childid);
 
     try {
+        const AllPlaylistsOfChild = await Playlist.find({
+            child: req.params.childid,
+        })
 
 
 
-        const AllPlaylistsOfChild = await Playlist.find({ child: req.params.childid });
-        res.json(AllPlaylistsOfChild);
+        const sortedVideos = AllPlaylistsOfChild.sort((a, b) => {
+            // if (a.name.localeCompare(b.name)) return 1;
+            // // if (b.name.localeCompare(a.name)) return -1;
+
+            if (a.name.toUpperCase < b.name.toUpperCase) {
+                return -1;
+            } else if (a.name.toUpperCase > b.name.toUpperCase) {
+                return 1;
+            } else {
+                // If the lowercase versions are equal, compare the original strings
+                if (a.name < b.name) {
+                    return -1;
+                } else if (a.name > b.name) {
+                    return 1;
+                } else {
+                    return 0;
+                }
+            }
+
+
+
+
+
+
+
+
+
+            console.log(a.name.localeCompare(b.name))
+        });
+
+
+
+        console.log(AllPlaylistsOfChild)
+
+
+
+
+        res.json(sortedVideos);
         // console.log(AllPlaylistsOfChild);
     } catch (error) {
         next(error);
     }
-})
+});
 
 //POST /api/playlist/new/:childid - Create a new playlist of a specific child
 router.post("/new/:childid", isAuthenticated, async (req, res, next) => {
     try {
         const newPlaylist = await Playlist.create({
             name: req.body.name,
-            child: req.params.childid
+            child: req.params.childid,
         });
         res.json(newPlaylist);
     } catch (error) {
         next(error);
     }
-})
+});
 
 // GET /api/playlist/:playlistid - Get information about a specific playlist
 router.get("/:playlistid", isAuthenticated, async (req, res, next) => {
@@ -40,98 +79,94 @@ router.get("/:playlistid", isAuthenticated, async (req, res, next) => {
     } catch (error) {
         next(error);
     }
-})
+});
 
 //DELETE /api/playlist/:playlistid - Delete a specific playlist
 router.delete("/:playlistid", isAuthenticated, async (req, res, next) => {
     try {
-        const deletedPlaylist = await Playlist.findByIdAndDelete(req.params.playlistid);
+        const deletedPlaylist = await Playlist.findByIdAndDelete(
+            req.params.playlistid
+        );
         res.json(deletedPlaylist);
     } catch (error) {
         next(error);
     }
-})
+});
 
 //PUT /api/playlist/:playlistid - Update a specific playlist
 router.put("/:playlistid", isAuthenticated, async (req, res, next) => {
-
     //! CHECK THIS PART SO VIDEOS ARE STORED PROPERLY
     // console.log(req.body)
     try {
-        const newPlaylistInfo = await Playlist.findByIdAndUpdate(req.params.playlistid, req.body, { new: true });
+        const newPlaylistInfo = await Playlist.findByIdAndUpdate(
+            req.params.playlistid,
+            req.body,
+            { new: true }
+        );
         res.json(newPlaylistInfo);
     } catch (error) {
         next(error);
     }
-})
+});
 
 // GET /api/playlist/videos/:playlistid - Get all videos of a specific playlist
 router.get("/videos/:playlistId", isAuthenticated, async (req, res, next) => {
-
-
-
     try {
-        const AllVideosOfPlaylist = await Playlist.findById(req.params.playlistId).populate("video");
+        const AllVideosOfPlaylist = await Playlist.findById(
+            req.params.playlistId
+        ).populate("video");
 
-
-        console.log("id of playlist: ", AllVideosOfPlaylist)
+        console.log("id of playlist: ", AllVideosOfPlaylist);
         res.json(AllVideosOfPlaylist);
     } catch (error) {
         next(error);
     }
-})
+});
 
 //GET /api/playlist/oneVideo/:playlistid/- Get one video of a specific playlist
 
 router.get("/oneVideo/:playlistId", isAuthenticated, async (req, res, next) => {
     try {
-        const OneVideoFromPlaylist = await Playlist.findById(req.params.playlistId).populate("video");
+        const OneVideoFromPlaylist = await Playlist.findById(
+            req.params.playlistId
+        ).populate("video");
         // console.log(OneVideoFromPlaylist.video[0].link)
         res.json(OneVideoFromPlaylist.video[0].link);
     } catch (error) {
         next(error);
     }
-})
-
+});
 
 //PUT /api/playlist/name/:playlistid - Update a specific playlist name
 router.put("/name/:playlistid", isAuthenticated, async (req, res, next) => {
-
-
-    console.log(req.body.name)
-    console.log(req.params.playlistid)
-
+    console.log(req.body.name);
+    console.log(req.params.playlistid);
 
     try {
-        const newPlaylistInfo = await Playlist.findByIdAndUpdate(req.params.playlistid, { name: req.body.name }, { new: true });
+        const newPlaylistInfo = await Playlist.findByIdAndUpdate(
+            req.params.playlistid,
+            { name: req.body.name },
+            { new: true }
+        );
 
-        console.log(newPlaylistInfo)
+        console.log(newPlaylistInfo);
 
         res.json(newPlaylistInfo);
     } catch (error) {
         next(error);
     }
-})
+});
 
 //GET /api/playlist/number/:playlistid - Update a specific playlist name
-router.get("/playlist/number/:playlistid", isAuthenticated, async (req, res, next) => {
-
-    try {
-
-
-
-
-
-    } catch (error) {
-        next(error);
+router.get(
+    "/playlist/number/:playlistid",
+    isAuthenticated,
+    async (req, res, next) => {
+        try {
+        } catch (error) {
+            next(error);
+        }
     }
+);
 
-
-
-
-
-})
-
-
-
-module.exports = router
+module.exports = router;
